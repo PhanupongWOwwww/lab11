324888ec2c2dd1c335cbe53bdb6b5c9931366ade
PhanupongWOwwww <Zozackgt@gmail.com>
2026-01-06 20:22:28 +0700

    lab11_1.cpp completed

--- lab11_1.cpp
+++ lab11_1.cpp
@@ -1,2 +1,22 @@
-// cout << "Press Enter 3 times to reveal your future.";
-// cout << "You will get A in this 261102.";
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+
+int main() {
+    string grades[] = {"A", "B+", "B", "C+", "C", "D+", "D", "F", "W"};
+    int size = 9;
+
+    cout << "Press Enter 3 times to reveal your future." << endl;
+
+    for (int i = 0; i < 3; i++) {
+        cin.get();
+    }
+
+    srand(time(0));
+    int luktao = rand() % size;
+
+    cout << "\nYou will get " << grades[luktao] << " in this 261102." << endl;
+
+    return 0;
+}
